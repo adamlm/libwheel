e90b8a80712b30f092eb7c5ef5c402d544a8eb2c
Adam Morrissett <me@adamlm.com>
2023-05-06 12:30:46 -0400

    #27 Add GTest and metaprogramming dependencies
    The generic vector implementation depends on the libwheel::metaprogramming
    library, and the unit tests depend on GTest

--- dependencies.cmake
+++ dependencies.cmake
@@ -1,1 +1,14 @@
 find_package(Boost COMPONENTS Graph REQUIRED)
+
+if(wheel_motion_planning_ENABLE_TESTING OR PROJECT_IS_TOP_LEVEL)
+  find_package(GTest REQUIRED)
+endif()
+
+include(FetchContent)
+
+FetchContent_Declare(libwheel_metaprogramming
+  GIT_REPOSITORY git@github.com:adamlm/libwheel-metaprogramming.git
+  GIT_TAG main
+)
+
+FetchContent_MakeAvailable(libwheel_metaprogramming)
