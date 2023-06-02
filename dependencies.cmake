44dbee4bbf89a93d535bed4d69d895c9e079d40f
Adam Morrissett <me@adamlm.com>
2023-06-02 14:46:11 +0000

    #11 Add documentation skeleton
    The documentation generation will use Sphinx with a plugin for
    connecting with Doxygen. It would be nice to use Material for MkDocs,
    but there doesn't seem to be a good plugin for Doxygen interfacing.

--- dependencies.cmake
+++ dependencies.cmake
@@ -1,6 +1,10 @@
 find_package(Eigen3 REQUIRED)
 find_package(GTest REQUIRED)
 
+if(wheel_strongly_typed_matrix_BUILD_DOCS OR PROJECT_IS_TOP_LEVEL)
+  find_package(Doxygen REQUIRED)
+endif()
+
 include(FetchContent)
 
 FetchContent_Declare(libwheel_metaprogramming
