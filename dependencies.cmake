3ee7423e39cfc7d08a2491effb2a78e252973d17
Adam Morrissett <me@adamlm.com>
2023-06-18 20:55:09 +0000

    #75 Add custom vector type examples
    These examples show how to integrate different vector types into the
    library. The custom_space example file has been removed because it no
    longer adhered to the library interface. The project dependencies have
    also been updated to only pull example and test dependencies when
    necessary.

--- dependencies.cmake
+++ dependencies.cmake
@@ -1,10 +1,8 @@
-find_package(Boost COMPONENTS Graph REQUIRED)
-
-if(wheel_motion_planning_ENABLE_TESTING OR PROJECT_IS_TOP_LEVEL)
-  find_package(GTest REQUIRED)
-endif()
-
 include(FetchContent)
+
+
+# Library dependencies
+find_package(Boost COMPONENTS Graph REQUIRED)
 
 FetchContent_Declare(libwheel_metaprogramming
   GIT_REPOSITORY git@github.com:adamlm/libwheel-metaprogramming.git
@@ -12,3 +10,22 @@ FetchContent_Declare(libwheel_metaprogramming
 )
 
 FetchContent_MakeAvailable(libwheel_metaprogramming)
+
+
+# Unit testing dependencies
+if(wheel_motion_planning_ENABLE_TESTING OR PROJECT_IS_TOP_LEVEL)
+  find_package(GTest REQUIRED)
+endif()
+
+
+# Example code dependencies
+if(wheel_motion_planning_BUILD_EXAMPLES OR PROJECT_IS_TOP_LEVEL)
+  find_package(Eigen3 REQUIRED)
+
+  FetchContent_Declare(libwheel_strongly_typed_matrix
+    GIT_REPOSITORY git@github.com:adamlm/libwheel-strongly_typed_matrix.git
+    GIT_TAG main
+  )
+
+  FetchContent_MakeAvailable(libwheel_strongly_typed_matrix)
+endif()
