81c86fdb1257e0956638b49cd7b563439e4273fe
Adam Morrissett <me@adamlm.com>
2023-09-04 00:17:59 -0400

    #36 Update Angle library CMake config
    The Wheel Angle library now has an updated CMake configuration similar
    to the other Wheel libraries.

---
+++ angle/options.cmake
@@ -0,0 +1,24 @@
+option(libwheel_angle_BUILD_LIB
+  "Build the library"
+  ${libwheel_BUILD_LIBS}
+)
+
+option(libwheel_angle_BUILD_TESTS
+  "Build the library's tests"
+  ${libwheel_BUILD_TESTS}
+)
+
+option(libwheel_angle_BUILD_INSTALL
+  "Build the library's CMake install targets"
+  ${libwheel_BUILD_INSTALL}
+)
+
+option(libwheel_angle_BUILD_DOCS
+  "Build the library's documentation"
+  ${libwheel_BUILD_DOCS}
+)
+
+option(libwheel_angle_BUILD_EXAMPLES
+  "Build the library's examples"
+  ${libwheel_BUILD_EXAMPLES}
+)
