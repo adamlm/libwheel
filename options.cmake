0aa0c7e7d1e07f2dd8dad66ed4dd0ed0e8543195
Adam Morrissett <me@adamlm.com>
2023-09-04 00:04:25 -0400

    #36 Move/add top-level options
    The top-level CMake options now live in a separate options.cmake
    file. These options provide default values for all libraries.

---
+++ options.cmake
@@ -0,0 +1,33 @@
+option(libwheel_EXPORT_COMPILE_COMMANDS
+  "Export the Wheel Library CMake compile commands"
+  ${PROJECT_IS_TOP_LEVEL}
+)
+
+option(libwheel_BUILD_LIBS
+  "Build all Wheel Library libraries by default"
+  ${PROJECT_IS_TOP_LEVEL}
+)
+
+option(libwheel_BUILD_TESTS
+  "Build all Wheel Library tests by default"
+  ${PROJECT_IS_TOP_LEVEL}
+)
+
+option(libwheel_BUILD_INSTALL
+  "Build all Wheel Library CMake install targets by default"
+  ${PROJECT_IS_TOP_LEVEL}
+)
+
+option(libwheel_BUILD_DOCS
+  "Build all Wheel Library documentation by default"
+  ${PROJECT_IS_TOP_LEVEL}
+)
+
+option(libwheel_BUILD_EXAMPLES
+  "Build all Wheel Library examples by default"
+  ${PROJECT_IS_TOP_LEVEL}
+)
+
+option(libwheel_BUILD_PACKAGING
+  "Build all Wheel Library packaging artifacts by default"
+  ${PROJECT_IS_TOP_LEVEL})
