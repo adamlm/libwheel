33ce40457dc01cfb0f00cfdbd577712b3d67b73d
Adam Morrissett <me@adamlm.com>
2024-03-11 01:57:15 +0000

    #83 Add options for building individual libraries

--- options.cmake
+++ options.cmake
@@ -30,4 +30,70 @@ option(libwheel_BUILD_EXAMPLES
 
 option(libwheel_BUILD_PACKAGING
   "Build all Wheel Library packaging artifacts by default"
-  ${PROJECT_IS_TOP_LEVEL})
+  ${PROJECT_IS_TOP_LEVEL}
+)
+
+option(libwheel_BUILD_PACKAGING
+  "Build all Wheel Library packaging artifacts by default"
+  ${PROJECT_IS_TOP_LEVEL}
+)
+
+option(libwheel_algorithm_BUILD_LIB
+  "Build the Wheel Algorithm library"
+  ${libwheel_BUILD_LIBS}
+)
+
+option(libwheel_angle_BUILD_LIB
+  "Build the Wheel Angle library"
+  ${libwheel_BUILD_LIBS}
+)
+
+option(libwheel_boost_graph_extensions_BUILD_LIB
+  "Build the Wheel Boost Graph Extensions library"
+  ${libwheel_BUILD_LIBS}
+)
+
+option(libwheel_controls_BUILD_LIB
+  "Build the Wheel Controls library"
+  ${libwheel_BUILD_LIBS}
+)
+
+option(libwheel_dimensioned_algebra_BUILD_LIB
+  "Build the Wheel Dimensioned Algebra library"
+  ${libwheel_BUILD_LIBS}
+)
+
+option(libwheel_exception_BUILD_LIB
+  "Build the Wheel Exception library"
+  ${libwheel_BUILD_LIBS}
+)
+
+option(libwheel_geometry_BUILD_LIB
+  "Build the Wheel Geometry library"
+  ${libwheel_BUILD_LIBS}
+)
+
+option(libwheel_math_BUILD_LIB
+  "Build the Wheel Math library"
+  ${libwheel_BUILD_LIBS}
+)
+
+option(libwheel_metaprogramming_BUILD_LIB
+  "Build the Wheel Metaprogramming library"
+  ${libwheel_BUILD_LIBS}
+)
+
+option(libwheel_motion_planning_BUILD_LIB
+  "Build the Wheel Motion Planning library"
+  ${libwheel_BUILD_LIBS}
+)
+
+option(libwheel_optimization_BUILD_LIB
+  "Build the Wheel Optimization library"
+  ${libwheel_BUILD_LIBS}
+)
+
+option(libwheel_strongly_typed_matrix_BUILD_LIB
+  "Build the Wheel Strongly-Typed Matrix library"
+  ${libwheel_BUILD_LIBS}
+)
