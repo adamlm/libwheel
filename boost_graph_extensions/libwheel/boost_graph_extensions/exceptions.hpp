0931921a7fcb535d7015f0411f8a2203cb5ad6a6
Adam Morrissett <me@adamlm.com>
2024-03-16 23:02:22 +0000

    #82 Add early search termination exception
    This exception type is being moved from the Motion Planning library.

---
+++ boost_graph_extensions/libwheel/boost_graph_extensions/exceptions.hpp
@@ -0,0 +1,27 @@
+#ifndef LIBWHEEL_BOOST_GRAPH_EXTENSIONS_HPP
+#define LIBWHEEL_BOOST_GRAPH_EXTENSIONS_HPP
+
+#include <exception>
+
+#include "libwheel/boost_graph_extensions/concepts.hpp"
+
+namespace wheel::boost_graph_extensions {
+
+template <boost_graph Graph>
+class early_search_termination : public std::runtime_error {
+  public:
+    explicit early_search_termination(std::string const &what_arg, vertex_descriptor_t<Graph> vertex)
+        : early_search_termination{what_arg.c_str(), vertex} {}
+
+    explicit early_search_termination(char const *what_arg, vertex_descriptor_t<Graph> vertex)
+        : std::runtime_error{what_arg}, vertex_{vertex} {}
+
+    auto get_last_vertex() const noexcept -> vertex_descriptor_t<Graph> { return vertex_; }
+
+  private:
+    vertex_descriptor_t<Graph> vertex_;
+};
+
+} // namespace wheel::boost_graph_extensions
+
+#endif // LIBWHEEL_BOOST_GRAPH_EXTENSIONS_HPP
