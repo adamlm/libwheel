0b4f2e1b8e64a05c1aa6cb7d2797dcdaac1ba38b
Adam Morrissett <me@adamlm.com>
2024-03-22 02:38:33 +0000

    Update straight line planner to accept a maximum distance parameter
    The class ahs also been renamed to use PascalCase to keep with a
    consistent naming convention.

--- motion_planning/libwheel/motion_planning/local_planning.hpp
+++ motion_planning/libwheel/motion_planning/local_planning.hpp
@@ -5,16 +5,17 @@
 
 #include "libwheel/motion_planning/detail/find_stopping_point.hpp"
 #include "libwheel/motion_planning/detail/interpolate_between.hpp"
+#include "libwheel/motion_planning/param_types.hpp"
 
 namespace wheel::motion_planning {
 
-namespace detail {
-
-struct straight_line_planner {
+struct StraightLinePlanner {
+  public:
+    explicit StraightLinePlanner(MaxDistance max_distance) : max_distance_{max_distance} {}
 
     auto operator()(auto const &source, auto const &target) const {
-        auto const exceeds_max_distance = [&source](auto const &point) {
-            return boost::geometry::distance(source, point) >= 1.0;
+        auto const exceeds_max_distance = [this, &source](auto const &point) {
+            return boost::geometry::distance(source, point) >= max_distance_.value;
         };
 
         auto const stopping_point{detail::find_stopping_point(
@@ -22,11 +23,10 @@ struct straight_line_planner {
 
         return std::vector{stopping_point.value()};
     }
+
+  private:
+    MaxDistance max_distance_{0.0};
 };
-
-} // namespace detail
-
-inline constexpr detail::straight_line_planner straight_line_planner{};
 
 } // namespace wheel::motion_planning
 
